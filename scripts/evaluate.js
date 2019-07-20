fdab2414f5ac3b44b56a8d486e4dea6903ce0836
Sakari Woods <sakariwoods@gmail.com>
2019-07-20 09:14:27 -0700

    Added selection-text-only capability, cleaned up console logs, will start addressing default behaviors.

--- scripts/evaluate.js
+++ scripts/evaluate.js
@@ -14,19 +14,32 @@ function createCORSRequest(method, url) {
 
 // Find the actual element needed to modify.
 var contenttosend;
-if(window.location.host == "mail.google.com"){
-    console.log("We're on google!");
-    foundElement = document.getElementsByClassName("Am Al editable LW-avf");
-    contenttosend = foundElement[0].innerHTML;
-}
-else if(window.location.host == "www.facebook.com"){ //WIP
-    foundElement = document.getElementsByClassName("notranslate _5rpu");
-    contenttosend = foundElement[0].innerHTML;
-    contenttosend = contenttosend.replace();
-}
+var foundElement;
+// If text is selected and in the buffer, use that instead.
+var sel, range;
+    if (window.getSelection().toString() != "") {
+        //console.log("Text is selected, converting that.");
+        contenttosend = window.getSelection().toString();
+    }
 else{
-    foundElement = document.activeElement;
-    contenttosend = foundElement.value;
+    //console.log("No text selected, finding ActiveElement");
+    // Override for finding default google email body.
+    if(window.location.host == "mail.google.com"){
+        //console.log("We're on google!");
+        foundElement = document.getElementsByClassName("Am Al editable LW-avf");
+        contenttosend = foundElement[0].innerHTML;
+    }
+    // Override for finding facebook post window.
+    else if(window.location.host == "www.facebook.com"){ //WIP
+        foundElement = document.getElementsByClassName("notranslate _5rpu");
+        contenttosend = foundElement[0].innerHTML;
+        contenttosend = contenttosend.replace();
+    }
+    else{
+        // Default behavior of focusing on activeElement.
+        foundElement = document.activeElement;
+        contenttosend = foundElement.value;
+    }
 }
 
 // Parse content divs out of text.
@@ -38,12 +51,12 @@ catch(err){
 }
 
 // Make the actual CORS request.
-console.log("sending "+contenttosend);
+//console.log("sending "+contenttosend);
 function makeCorsRequest() {
   var url = 'https://h0h46kcpo6.execute-api.us-east-2.amazonaws.com/Prod?text='+contenttosend;
   var xhr = createCORSRequest('GET', url);
   if (!xhr) {
-    console.log("CORS not supported");
+    //console.log("CORS not supported");
     return;
   }
 
@@ -51,27 +64,42 @@ function makeCorsRequest() {
   xhr.onload = function() {
       if(content != null || content != ""){
           var content = JSON.parse(xhr.responseText).text;
-          console.log("Received: "+content);
+          //console.log("Received: "+content);
 
 // Content edits before insertion back into website.
 // Potential code goes here.
 //**************************
 
 // Set text back and edit the content.
-          if(window.location.host == "mail.google.com"){
-              foundElement[0].innerHTML = content;
-          }
-          else if(window.location.host == "www.facebook.com"){
-              foundElement[0].innerHTML = content;
-          }
-          else{
-              foundElement.value = content;
-          }
+if (window.getSelection().toString() != "") {
+    //console.log("Placing selected text back...");
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+        range = sel.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(document.createTextNode(content));
+    }
+}
+else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    range.text = content;
+}
+else{
+    if(window.location.host == "mail.google.com"){
+        foundElement[0].innerHTML = content;
+    }
+    else if(window.location.host == "www.facebook.com"){
+        foundElement[0].innerHTML = content;
+    }
+    else{
+        foundElement.value = content;
+    }
+}
       }
   };
 
   xhr.onerror = function() {
-    console.log("Request error");
+    //console.log("Request error");
   };
   xhr.send();
 }
