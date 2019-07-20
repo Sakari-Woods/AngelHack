a2a3439a02a313b6b0111f0b6a0664e6edb45aed
Sakari Woods <sakariwoods@gmail.com>
2019-07-20 08:37:00 -0700

    Hard-coded cors fix, merge from Asimov4

--- serverlessrepo-cedille-api/routes/root.js
+++ serverlessrepo-cedille-api/routes/root.js
@@ -3,19 +3,19 @@ const config = require("../cors-config.json");
 
 /**
  * Demonstrates a simple endpoint that accepts GET requests.
- * 
+ *
  * In most cases, browsers do not perform CORS preflight requests when using
  * the GET method, so we do not have to handle OPTIONS requests.
  * Any desired additional CORS headers should be included in the GET response.
  */
 exports.handler = async (event, context) => {
     const origin = cors.getOriginFromEvent(event);
-    
+
     console.log("\nInput text: " + event.queryStringParameters.text);
 
     // return an empty response, with CORS origin
     return {
-        headers: cors.createOriginHeader(origin, config.allowedOrigins),
+        headers: {"Access-Control-Allow-Origin": "*"},
         statusCode: 200,
         body: JSON.stringify({ text: event.queryStringParameters.text })
     };
